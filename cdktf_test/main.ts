a58fedf8c46007393e1d55932b99a00132740116
Ayman Zahran <ah.zahran@outlook.com>
2022-10-08 21:19:46 +0000

    Commit

---
+++ cdktf_test/main.ts
@@ -0,0 +1,34 @@
+import { Construct } from "constructs";
+import { App, TerraformStack, TerraformOutput, CloudBackend, NamedCloudWorkspace } from "cdktf";
+import { AwsProvider, ec2 } from "@cdktf/provider-aws";
+
+class MyStack extends TerraformStack {
+  constructor(scope: Construct, name: string) {
+    super(scope, name);
+
+    new AwsProvider(this, "AWS", {
+      region: "us-west-1",
+    });
+
+    const instance = new ec2.Instance(this, "compute", {
+      ami: "ami-01456a894f71116f2",
+      instanceType: "t2.micro",
+    });
+
+    new TerraformOutput(this, "public_ip", {
+      value: instance.publicIp,
+    });
+  }
+}
+
+const app = new App();
+const stack = new MyStack(app, "cdktf_test");
+new CloudBackend(stack, {
+  hostname: "app.terraform.io",
+  organization: "Ayman-Organization",
+  workspaces: new NamedCloudWorkspace("cdktf_test")
+});
+
+app.synth();
+
+
