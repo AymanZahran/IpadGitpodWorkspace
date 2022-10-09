7527ee3a309fff8ad4f569a7e5c0e7f114374b6e
Ayman Zahran <ah.zahran@outlook.com>
2022-10-09 21:24:30 +0000

    Commit

--- Infrastructure/src/main.ts
+++ Infrastructure/src/main.ts
@@ -1,11 +1,21 @@
-import { App, Stack, StackProps } from 'aws-cdk-lib';
+import { App, Stack, StackProps, aws_eks, aws_ec2 } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
 export class MyStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps = {}) {
     super(scope, id, props);
 
-    // define resources here...
+    const cluster = new aws_eks.Cluster(this, 'HelloEKS', {
+      version: aws_eks.KubernetesVersion.V1_21,
+    });
+
+    cluster.addNodegroupCapacity('custom-node-group', {
+      instanceTypes: [new aws_ec2.InstanceType('m5.xlarge')],
+      nodegroupName: 'MyNodeGroup',
+      maxSize: 3,
+      minSize: 3,
+      desiredSize: 3,
+    });
   }
 }
 
