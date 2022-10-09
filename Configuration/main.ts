87ccb096094d7084f5192aec32c0dbb6b30b90d2
Ayman Zahran <ah.zahran@outlook.com>
2022-10-09 09:17:54 +0000

    Commit

--- Configuration/main.ts
+++ Configuration/main.ts
@@ -1,53 +1,33 @@
 import { Construct } from 'constructs';
-import { App, Chart, ChartProps } from 'cdk8s';
-import { KubeDeployment, KubeService, IntOrString } from './imports/k8s';
+import { App, Chart, ChartProps, ApiObject } from 'cdk8s';
+import { Deployment } from 'cdk8s-plus-22';
 
 export class MyChart extends Chart {
   constructor(scope: Construct, id: string, props: ChartProps = { }) {
     super(scope, id, props);
 
-    const label = { app: 'hello-k8s' };
-
-    // notice that there is no assignment necessary when creating resources.
-    // simply instantiating the resource is enough because it adds it to the construct tree via
-    // the first argument, which is always the parent construct.
-    // its a little confusing at first glance, but this is an inherent aspect of the constructs
-    // programming model, and you will encounter it many times.
-    // you can still perform an assignment of course, if you need to access
-    // attributes of the resource in other parts of the code.
-
-    new KubeService(this, 'service', {
-      spec: {
-        type: 'LoadBalancer',
-        ports: [ { port: 80, targetPort: IntOrString.fromNumber(8080) } ],
-        selector: label
-      }
-    });
-
-    new KubeDeployment(this, 'deployment', {
-      spec: {
-        replicas: 2,
-        selector: {
-          matchLabels: label
-        },
-        template: {
-          metadata: { labels: label },
-          spec: {
-            containers: [
-              {
-                name: 'hello-kubernetes',
-                image: 'paulbouwer/hello-kubernetes:1.7',
-                ports: [ { containerPort: 8080 } ]
-              }
-            ]
-          }
-        }
-      }
-    });
-
   }
 }
 
 const app = new App();
-new MyChart(app, 'cdk8s_test');
+const tekton_chart = new MyChart(app, 'tekton');
+const wordpress_chart = new MyChart(app, 'wordpress');
+const mysql_chart = new MyChart(app, 'mysql');
+
+new Deployment(wordpress_chart, 'wordpress', {
+  containers: [ { image: 'aymanzahran/wordpress:4.8-apache' } ],
+  replicas: 3,
+});
+
+
+new Deployment(mysql_chart, 'mysql', {
+  containers: [ { image: 'aymanzahran/wordpress:4.8-apache' } ],
+  replicas: 3,
+});
+
+new ApiObject(tekton_chart, 'tekton', {
+  apiVersion: 'v1',
+  kind: 'Task'
+});
+
 app.synth();
