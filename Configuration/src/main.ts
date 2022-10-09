cbf8fd3412beb979ba4e0d0029ebf1dfcdb2fc16
Ayman Zahran <ah.zahran@outlook.com>
2022-10-09 09:39:30 +0000

    Commit

--- Configuration/src/main.ts
+++ Configuration/src/main.ts
@@ -1,6 +1,6 @@
-import { Construct } from 'constructs';
 import { App, Chart, ChartProps, ApiObject } from 'cdk8s';
 import { Deployment } from 'cdk8s-plus-22';
+import { Construct } from 'constructs';
 
 export class MyChart extends Chart {
   constructor(scope: Construct, id: string, props: ChartProps = { }) {
@@ -15,19 +15,19 @@ const wordpress_chart = new MyChart(app, 'wordpress');
 const mysql_chart = new MyChart(app, 'mysql');
 
 new Deployment(wordpress_chart, 'wordpress', {
-  containers: [ { image: 'aymanzahran/wordpress:4.8-apache' } ],
+  containers: [{ image: 'aymanzahran/wordpress:4.8-apache' }],
   replicas: 3,
 });
 
 
 new Deployment(mysql_chart, 'mysql', {
-  containers: [ { image: 'aymanzahran/wordpress:4.8-apache' } ],
+  containers: [{ image: 'aymanzahran/wordpress:4.8-apache' }],
   replicas: 3,
 });
 
 new ApiObject(tekton_chart, 'tekton', {
   apiVersion: 'v1',
-  kind: 'Task'
+  kind: 'Task',
 });
 
 app.synth();
