4664748d9559a4504ad137cf98feecdaa8c3afc8
Ayman Zahran <ah.zahran@outlook.com>
2022-10-10 20:26:44 +0000

    Commit

--- Infrastructure/src/main.ts
+++ Infrastructure/src/main.ts
@@ -1,21 +1,40 @@
-import { App, Stack, StackProps, aws_eks, aws_ec2 } from 'aws-cdk-lib';
+// import 'source-map-support/register';
+// import { App, Stack, StackProps, aws_eks, aws_ec2 } from 'aws-cdk-lib';
+import { App, Stack, StackProps} from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as blueprints from '@aws-quickstart/eks-blueprints';
 
 export class MyStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps = {}) {
     super(scope, id, props);
 
-    const cluster = new aws_eks.Cluster(this, 'HelloEKS', {
-      version: aws_eks.KubernetesVersion.V1_21,
-    });
+    const addOns: Array<blueprints.ClusterAddOn> = [
+      new blueprints.addons.ArgoCDAddOn,
+      new blueprints.addons.CalicoAddOn,
+      new blueprints.addons.MetricsServerAddOn,
+      new blueprints.addons.ClusterAutoScalerAddOn,
+      new blueprints.addons.ContainerInsightsAddOn,
+      new blueprints.addons.AwsLoadBalancerControllerAddOn(),
+      new blueprints.addons.VpcCniAddOn(),
+      new blueprints.addons.CoreDnsAddOn(),
+      new blueprints.addons.KubeProxyAddOn(),
+      new blueprints.addons.XrayAddOn()
+    ];
+    
+    new blueprints.EksBlueprint(app, { id: 'east-test-1', addOns}, props)
 
-    cluster.addNodegroupCapacity('node-group', {
-      instanceTypes: [new aws_ec2.InstanceType('m5.xlarge')],
-      nodegroupName: 'node-group',
-      maxSize: 3,
-      minSize: 3,
-      desiredSize: 3,
-    });
+
+    // const cluster = new aws_eks.Cluster(this, 'HelloEKS', {
+    //   version: aws_eks.KubernetesVersion.V1_21,
+    // });
+
+    // cluster.addNodegroupCapacity('node-group', {
+    //   instanceTypes: [new aws_ec2.InstanceType('m5.xlarge')],
+    //   nodegroupName: 'node-group',
+    //   maxSize: 3,
+    //   minSize: 3,
+    //   desiredSize: 3,
+    // });
   }
 }
 
