7bcf3f92c9727352f31ab8e6eff18a4257ebc369
Ayman Zahran <ah.zahran@outlook.com>
2022-10-10 21:10:29 +0000

    Commit

--- Infrastructure/.projenrc.js
+++ Infrastructure/.projenrc.js
@@ -4,13 +4,9 @@ const project = new awscdk.AwsCdkTypeScriptApp({
   defaultReleaseBranch: 'main',
   name: 'Infrastructure',
 
-  deps: [
-    '@aws-quickstart/eks-blueprints',
-  ], /* Runtime dependencies of this module. */
+  // deps: [], /* Runtime dependencies of this module. */
   // // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  devDeps: [
-    '@aws-quickstart/eks-blueprints',
-  ], /* Build dependencies for this module. */
+  // devDeps: [], /* Build dependencies for this module. */
   // // packageName: undefined,  /* The "name" in package.json. */
   github: false, /* Add GitHub workflows. */
 });
